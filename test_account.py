26e5060d4e13f03f710338f17291edf871e66eb3
mpatrick411 <milespatrick01@gmail.com>
2023-04-21 17:09:47 -0500

    Test file

---
+++ test_account.py
@@ -0,0 +1,29 @@
+from account import *
+
+
+class Test:
+    def setup_method(self):
+        self.a1 = Account('John')
+
+    def teardown_method(self):
+        del self.a1
+
+    def test_init(self):
+        pass
+
+    def test_deposit(self):
+        assert self.a1.deposit(-10) == False
+        assert self.a1.deposit(0) == False
+        assert self.a1.deposit(10) == True
+        pass
+
+    def test_withdraw(self):
+        self.a1.deposit(5)
+        assert self.a1.withdraw(-10) == False
+        assert self.a1.withdraw(0) == False
+        assert self.a1.withdraw(10) == False
+        assert self.a1.withdraw(1) == True
+        pass
+
+
+
